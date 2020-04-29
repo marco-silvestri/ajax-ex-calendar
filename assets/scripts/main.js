40d245022c1a5a35bb96ecfd0dfbad9776278f64
Marco Silvestri <m.silvestri03@gmail.com>
2020-04-29 19:02:19 +0200

    Print calendar

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,13 +1,49 @@
 $(document).ready(function () {
-    /*
-    *   Print the calendar
-    */
-
     //  Load a default date on start
     var dateOnLoad = moment('2018-01-01');
 
-    // Init template with HANDLEBARS
-    var source = $('#template-calendar');
+    //  Init template with HANDLEBARS
+    var source = $('#template-calendar').html();
     var template = Handlebars.compile(source);
 
+    var calendarApp = $('#calendar');
+    var calendarMonth = $('.calendar__month');
+
+    printMonth(dateOnLoad, calendarMonth, template, calendarApp);
+
 });
+
+/**************
+ *  FUNCTIONS
+ **************/
+
+function printMonth(date, month, template, destination){
+    //  Retrieve the days in a month
+    var daysinMonth = date.daysInMonth();
+
+    //  Retrieve the literal month and the year
+    var monthAndYear = date.format('MMMM YYYY');
+
+    month.text(monthAndYear);
+
+    //  Print the dates
+    for (var i = 1; i <= daysinMonth; i++){
+        var thisDate = moment({
+            year : date.year(),
+            month : date.month(),
+            day : i
+        });
+
+        //  Set the template
+        var dateTemplate = {
+            thisDate : thisDate.format('DD'),
+            thisName : thisDate.format('ddd'),
+        }
+
+        //  Compile the template
+        var output = template(dateTemplate);
+        destination.append(output);
+
+    }
+
+};
